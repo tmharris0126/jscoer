c21ec0830de980d50782796da114dc79971cfd59
Terrell Harris <tmharris0126@gmail.com>
2017-03-09 02:45:48 -0600

    fnl

---
+++ main.js
@@ -0,0 +1,60 @@
+// parseInt(col, 1,2,3) : String Values
+
+// function ime() {
+//     var x = document.getElementById("inumber").rows.length;
+//     document.getElementById("ihere").innerHTML = "Found " + x + " tr elements in the table.";
+// }
+// function ime() {
+//     var x = document.getElementById("istring").rows.length;
+//     document.getElementById("ihere").innerHTML = "Found " + x + " tr elements in the table.";
+// }
+// function ime() {
+//     var x = document.getElementById("iboolean").rows.length;
+//     document.getElementById("ihere").innerHTML = "Found " + x + " tr elements in the table.";
+// }
+//
+// function math() {
+//     results.textContent = (Number(irow.value) + String(irow.value) + Boolean(irow.value));
+// }
+
+
+
+// var x = false;
+// document.getElementById("row") =
+// "Number : " + Number(x) + "<td>" +
+// "String : " + String(x) + "<td>" +
+// "Boolean: " + Boolean(x);
+
+
+// var nmber = document.querySelector('nmber')
+// console.dir(document.querySelector('#nmber'))
+// var string = document.querySelector('string')
+// console.dir(document.querySelector('#string'))
+// var boolean = document.querySelector('boolean')
+// console.dir(document.querySelector('#boolean'))
+
+
+function functionName() {
+  document.getElementById('irow')
+console.log (undefined === 'number');
+console.log (null === 'number');
+console.log (true === 'number');
+console.log (false === 'number');
+console.log ("" === 'number');
+console.log ("1.2" === 'number');
+console.log ("one" === 'number');
+console.log (0 == 'number');
+console.log (-0 === 'number');
+console.log (NaN === 'number');
+console.log (Infinity === 'number');
+console.log (-Infinity === 'number');
+console.log (1 === 'number');
+console.log ({} === 'number');
+console.log ([] === 'number');
+console.log ([12] === 'number');
+console.log (['something'] === 'number');
+console.log (function(){} === 'number');
+}
+
+    // var x = document.getElementById("iboolean");
+    // document.getElementById("ihere").innerHTML = "Found " + x + " tr elements in the table.";
